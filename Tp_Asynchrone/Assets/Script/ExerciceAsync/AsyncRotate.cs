7ceb56982e3ecb3adeaf1747aa12ddca94f127e5
Eva291204 <eva.fricaud@e-artsup.net>
2024-02-08 11:46:47 +0100

    TP Asynchrone

---
+++ Tp_Asynchrone/Assets/Script/ExerciceAsync/AsyncRotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class AsyncRotate : MonoBehaviour
+{
+    public int Speed;
+    public GameObject Cube;
+    [SerializeField] private bool _isRotate;
+    private CancellationTokenSource _cancellationToken;
+
+    public void OnRotate()
+    {
+        _cancellationToken = new CancellationTokenSource();
+        RotateCube();
+    }
+    public void StopRotate()
+    {
+        _cancellationToken.Cancel();
+    }
+    public async void RotateCube()
+    {
+        _isRotate = true;
+        await UniTask.Delay(5000, cancellationToken: _cancellationToken.Token).SuppressCancellationThrow();
+        _isRotate = false;
+    }
+
+    public void Update()
+    {
+        if (_isRotate)
+        {
+            Cube.transform.Rotate(90, 90, 90 * Speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Tp_Asynchrone/Assets/Script/ExerciceCoroutine/CoroutineRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineRotate : MonoBehaviour
+{
+    public int Speed;
+    public GameObject Cube;
+    private Coroutine _rotate;
+    public void OnRotate()
+    {
+        _rotate = StartCoroutine(RotateForFiveSecond());
+    }
+
+    public void OnStopRotate()
+    {
+        StopCoroutine(_rotate);
+    }
+
+    IEnumerator RotateForFiveSecond()
+    {
+        float timeToSpin = 0;
+        while (timeToSpin <= 4)
+        {
+            Cube.transform.Rotate(90, 90, 90 * Speed * Time.deltaTime);
+            timeToSpin += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
